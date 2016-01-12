ca208d0b7eec0661d2c9d7e89895fdb989c59f91
ganhaitian <ganhaitian@gmail.com>
2016-01-12 22:49:27 +0800

    用户提交
    用户提交

---
+++ src/main/java/com/firstsnow/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.firstsnow.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 用户
+ * Created by lenovo on 2016/1/8.
+ */
+@Controller
+public class UserController {
+
+
+
+}
